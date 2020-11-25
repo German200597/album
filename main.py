8b9b6959bc7a770d9deb78e2349c24d3ae178dcb
German200597 <63213547+German200597@users.noreply.github.com>
2020-11-26 01:25:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Track:
     self.duration = int(duration) 
   def show(self):
     print(f'{self.track_title} - {self.duration} минуты') 
+  
 
 track_one = Track('первый трек', 1)
 track_two = Track('второй трек', 2)
@@ -21,22 +22,25 @@ track_six = Track('шестой трек', 4)
 # track_four.show()
 
 class Album(Track): 
-  def __init__(self, album_title, band):
-    self.album_title = album_title
-    self.band = band
-    self.tracks_list = []
+   def __init__(self, album_title, band):
+     self.album_title = album_title
+     self.band = band
+     self.tracks_list = []
 
-  def add_tracks(self, names):
-    self.tracks_list.append(names)
-    return(self.tracks_list)
-  def get_duration(self):
-    print(f'Длина альбома «{self.album_title}» — {sum(self.tracks_list[1])} минут')
-  def get_tracks(self):
-    print(f'Альбом: {self.album_title} ')
-    print(f'Группа: {self.band}')
-    for i in self.tracks_list:
-      print(f'Трек: {i.track_title} - {i.duration} мин')
- 
+   def add_tracks(self, names):
+     self.tracks_list.append(names)
+     return(self.tracks_list)
+   def get_duration(self):
+     summa = 0
+     for i in self.tracks_list:
+        summa += i.duration
+     print(f'Длина альбома «{self.album_title}» — {summa} минут')
+   def get_tracks(self):
+     print(f'Альбом: {self.album_title} ')
+     print(f'Группа: {self.band}')
+     for i in self.tracks_list:
+       print(f'Трек: {i.track_title} - {i.duration} мин')
+
 album_one = Album('Первый альбом', 'Первая группа')
 album_two = Album('Второй альбом', 'Вторая группа')
 album_one.add_tracks(track_one)
@@ -50,8 +54,8 @@ album_two.add_tracks(track_six)
 # print(album_two.tracks_list)
 album_one.get_tracks()
 album_two.get_tracks()
-# album_one.get_duration()
-# album_two.get_duration()
+album_one.get_duration()
+album_two.get_duration()
 
 
 
